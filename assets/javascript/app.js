40ec814cfe28571658b7ab7b0cf688b82953afaa
Katerine <katerinehw@att.net>
2018-11-13 17:45:31 -0800

    tuesday night

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+
+
+// set time out
+setTimeout(timeUp, 1000 *60);
+
+
+
+
+
+// functions
+function timeUp() {
+    $("#time-left").append("Time's up!</h2>")
+    console.log("Done!");
+     // in the element with an id of `time-left` add an h2 saying Time's Up!
+    // console log done
