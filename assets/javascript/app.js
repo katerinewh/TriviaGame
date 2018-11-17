74fe296a942133202c70ab7624aa6b3b2538b3f6
Katerine <katerinehw@att.net>
2018-11-16 18:30:34 -0800

    Fri after dinner

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,66 +5,73 @@ $(document).ready(function () {
             question: "What was the first movie to prompt a PG-13 rating?",
             answers: ["Rocky", "Stand By Me", "Ghostbusters", "Indiana Jones and the Temple of Doom"],
             correctAnswer: 3,
-
+            photo: "assets/images/IJTOD.jpg"
         },
         {
             question: "Which actor never appeared in the first four Batman movies?",
             answers: ["Kevin Spacey", "Drew Barrymore", "Christopher Walken", "Billy Dee Williams"],
             correctAnswer: 0,
-
+            photo: "assets/images/batman.jpg"
         },
         {
             question: "What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?",
             answers: ["Bilbo Baggins", "Samwise Gamgee", "Frodo Baggins", "Harry Goatleaf"],
             correctAnswer: 2,
+            photo: "assets/images/LOTR.jpg"
+
         },
         {
             question: " Which film includes these 4 actresses ? Maya Rudolph, Kristen Wiig, Rose Byrne and Melissa McCarthy.?",
             answers: ["Anchorman 2: The Legend Continues", "Ghostbusters ", "Life of the Party ", "Bridesmades"],
             correctAnswer: 3,
+            photo: "assets/images/bridesmaids.jpg"
 
         },
         {
             question: "What 1961 movie has Audrey Hepburn note: Personally, I think it's a bit tacky to wear diamonds before I'm 40?",
             answers: ["Sabrina", "Breakfast at Tiffany's", "Roman Holiday", "My Fair Lady"],
             correctAnswer: 1,
+            photo: "assets/images/tiffanys.jpg"
 
         },
         {
             question: "Which actress plays Katniss Everdeen in the Hunger Games movies?",
             answers: ["Jennifer Lawrence ", "Shailene Woodley", "Kiera Knightley", "Emma Watson"],
             correctAnswer: 0,
+            photo: "assets/images/hunger.jpg"
 
         },
         {
             question: "What is the name of the kingdom where the 2013 animated movie Frozen is set",
             answers: ["Arendelle", "Eruburg", "Arnor", "Gondor"],
             correctAnswer: 0,
-
+            photo: "assets/images/frozen.jpg"
         },
         {
             question: "Which classic thriller movie stars Roy Schieder as the police chief Martin Brody",
             answers: ["Rosemary's Baby", "Exorcist", "Holloween", "Jaws"],
             correctAnswer: 3,
-
+            photo: "assets/images/jaws.jpg"
         },
         {
             question: "Which Tom Hanks movie won the Academy Award for Best Picture in 1994?",
             answers: ["Cast Away", "Forrest Gump", "Green Mile ", "Philladelphia"],
             correctAnswer: 1,
+            photo: "assets/images/forrest.jpg"
 
         },
         {
             question: "Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones tells the story of a secret agency that polices alien refugees who are living on earth disguised as humans?",
             answers: ["Cowboys vs. Aliens ", "The Fugitive", "Men in Black", "Bad Boys"],
             correctAnswer: 2,
+            photo: "assets/images/MIB.jpg"
 
         }];
 
     var correctCount = 0;
     var wrongCount = 0;
     var unanswerCount = 0;
-    var timer = 20;
+    var timer = 10;
     var intervalId;
     var userGuess = "";
     var running = false;
@@ -150,7 +157,7 @@ $(document).ready(function () {
             console.log("Correct answer: ", pick.answers[pick.correctAnswer]);
             console.log("userGuess: ", userGuess);
 
-            //correct guess or wrong guess outcomes
+            //correct / incorrect//
             if (userGuess === pick.correctAnswer) {
                 stop();
                 correctCount++;
@@ -174,7 +181,7 @@ $(document).ready(function () {
     // scoring//
     function score() {
         $("#answerblock").append("<img src=" + pick.photo + ">");
-        newArray.push();
+        newArray.push(pick);
         options.splice(index, 1);
 
         var hidpic = setTimeout(function () {
@@ -197,21 +204,21 @@ $(document).ready(function () {
                 displayQuestion();
             }
 
-        }, 3000);
-
+        }, 1000);
+    }
         // reset function//
-
-        function reset() {
-            score();
+        $("#reset").on("click", function () {
             $("#reset").hide();
             $("#answerblock").empty();
             $("#questionblock").empty();
+            for(var i = 0; i < holder.length; i++) {
+                options.push(holder[i]);
+        }
             runTimer();
             displayQuestion();
 
-        }
-    }
+    })
 })
-    // function resetGame() {
-    //         }
+
+
 
